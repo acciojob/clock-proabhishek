15d8b90125574632d4d3798ae0cc92103d9bb64d
Abhishek Shankar Choudhary <28715657+proabhishek@users.noreply.github.com>
2024-04-23 22:10:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,64 +1,46 @@
-//your JS code here. If required.
-let para = document.querySelector("#timer")
+let para = document.getElementById("timer")
 
+function currentTime(){
+	let date = new Date()
+    let hr = date.getHours()
+    let minute = date.getMinutes()
+	minute = minute <=9 ? "0"+minute : minute
+    let second = date.getSeconds()
+    second = second <=9 ? "0"+second : second
+    let am_pm
 
-function currentTime() {
-	  let date = new Date()
-	  let hour = date.getHours()
-	  let min = date.getMinutes()
-	  let sec = date.getSeconds()
-	  let day = date.getDay()  
-      let month = date.getMonth()
-      let year = date.getFullYear()
-	   let arrayofTime = convertTime(hour, min, sec) 
-       // [hour, min, sec, AM/PM]
-       for(i = 0; i<=2; i++){
-        arrayofTime[i] = addZero(arrayofTime[i])
-       }
-    let time = `${arrayofTime[0]}:${arrayofTime[1]}:${arrayofTime[2]} ${arrayofTime[3]}`
-      para.innerHTML = `${month}/${day}/${year}, ${time} `
+	if(hr == 0){
+		hr = 12
+		am_pm = "AM"
+	}
+	else if(hr<12){
+		am_pm = "AM"
+	}
+	else if(hr == 12){
+		am_pm = "PM"
+	}
+	else if(hr>12){
+		hr = hr  - 12 
+		am_pm = "PM"
+	}
+	
+	
+	let dateDay = date.getDate() 
+    let month = date.getMonth()+1 // 0-11 
+    let year = date.getFullYear() 
 
-	// return `${month}/${day}/${year}, ${time} `
-}
-
-// let ans = currentTime()
-// alert(ans)
-
-setInterval(currentTime,1000)
-
-
-
-function convertTime(hr, min, sec){
-
-  //  11:59Pm = 23:59
-  // 12:00Am = 00:00
-
-    if(hr>12){
-      hr = hr - 12  
-    return [hr, min, sec, "PM"]
-    }
-    else if(hr == 0){
-        // return `12:${min}:${sec} AM`
-        return [12, min, sec, "AM"]
-    }
-    else if(hr<12){
-    //   return `${hr}:${min}:${sec} AM`
-      return [hr, min, sec, "AM"]
-    }
-    else if(hr == 12){
-        // return `${hr}:${min}:${sec} PM`
-        return [hr, min, sec, "PM"]
-    }
-    
+     
+	para.innerText = `${month}/${dateDay}/${year}, ${hr}:${minute}:${second} ${am_pm}`
 }
 
 
-function addZero(k){
-  if(k<10){
-    return "0"+k
-  }
-  else {
-    return k
-  }
-}
+setInterval(currentTime, 1000)
+
+
+
+
+
+
+
+
 
